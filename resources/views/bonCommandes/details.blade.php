b3ab19bc472fb238e0222a9cdb9d5b354151ca20
jonathandry32 <bayronkevin1@gmail.com>
2023-11-19 21:24:03 +0300

    en lettre

--- resources/views/bonCommandes/details.blade.php
+++ resources/views/bonCommandes/details.blade.php
@@ -86,7 +86,87 @@
                 </table>
             </div>
         </div>
+        <br/>
+        <h3> Bon de commande arrete a la somme de {{ convertirEnLettres($total) }} Ariary </h3>
         </div>
      </div>
 </div>
 @endsection
+@php
+function convertirEnLettres($chiffre)
+{
+    $unites = ['', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf'];
+    $dizaines = ['', 'dix', 'vingt', 'trente', 'quarante', 'cinquante', 'soixante', 'soixante-dix', 'quatre-vingt', 'quatre-vingt-dix'];
+
+    $chiffre = (int) $chiffre;
+
+    if ($chiffre < 0 || $chiffre > 999999999) {
+        return 'Valeur invalide';
+    }
+
+    $resultat = convertirTroisChiffres($chiffre % 1000, $unites, $dizaines);
+
+    $milliards = $chiffre / 1000000000;
+    $milliardsPartieEntiere = floor($milliards);
+    $milliardsReste = $chiffre % 1000000000;
+
+    if ($milliardsPartieEntiere > 0) {
+        $resultat = convertirTroisChiffres($milliardsPartieEntiere, $unites, $dizaines) . ' milliard' . ($milliardsPartieEntiere > 1 ? 's' : '');
+    }
+
+    $millions = $milliardsReste / 1000000;
+    $millionsPartieEntiere = floor($millions);
+    $millionsReste = $milliardsReste % 1000000;
+
+    if ($millionsPartieEntiere > 0) {
+        $resultat .= ($resultat ? ' ' : '') . convertirTroisChiffres($millionsPartieEntiere, $unites, $dizaines) . ' million' . ($millionsPartieEntiere > 1 ? 's' : '');
+    }
+
+    $milliers = $millionsReste / 1000;
+    $milliersPartieEntiere = floor($milliers);
+    $unitesRestantes = $millionsReste % 1000;
+
+    if ($milliersPartieEntiere > 0) {
+        $resultat .= ($resultat ? ' ' : '') . convertirTroisChiffres($milliersPartieEntiere, $unites, $dizaines) . ' mille';
+    }
+
+    if ($unitesRestantes > 0) {
+        $resultat .= ($resultat ? ' ' : '') . convertirTroisChiffres($unitesRestantes, $unites, $dizaines);
+    }
+
+    return ucfirst($resultat);
+}
+
+function convertirTroisChiffres($nombre, $unites, $dizaines)
+{
+    $resultat = '';
+
+    $centaines = floor($nombre / 100);
+    $reste = $nombre % 100;
+
+    if ($centaines > 0) {
+        $resultat .= $unites[$centaines] . ' cent';
+    }
+
+    if ($reste > 0) {
+        if ($centaines > 0) {
+            $resultat .= ' ';
+        }
+
+        if ($reste < 10) {
+            $resultat .= $unites[$reste];
+        } elseif ($reste < 20) {
+            $resultat .= 'dix-' . $unites[$reste % 10];
+        } else {
+            $resultat .= $dizaines[floor($reste / 10)];
+
+            $unite = $reste % 10;
+            if ($unite > 0) {
+                $resultat .= '-' . $unites[$unite];
+            }
+        }
+    }
+
+    return $resultat;
+}
+@endphp
